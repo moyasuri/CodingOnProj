43d2a03db42773c8f33576eca8981955cb1cd15e
moyasuri <moyasuri.hana@gmail.com>
2023-09-18 06:52:05 +0900

    0918-time

--- CodingOnProj/proj2_test.cpp
+++ CodingOnProj/proj2_test.cpp
@@ -15,35 +15,36 @@ int main() {
     INPUT_RECORD ir[128];
 
     // 시작 시간을 기록합니다.
-    DWORD startTime = GetTickCount();
-
+    DWORD startTime = GetTickCount64();
     while (true) {
-        // 현재 시간을 가져옵니다.
-        DWORD currentTime = GetTickCount();
+        while (true) {
+            // 현재 시간을 가져옵니다.
+            DWORD currentTime = GetTickCount64();
 
-        // 경과 시간을 계산합니다.
-        DWORD elapsedTime = currentTime - startTime;
+            // 경과 시간을 계산합니다.
+            DWORD elapsedTime = currentTime - startTime;
 
-        // 시간 제한을 초과하면 종료합니다.
-        if (elapsedTime >= timeLimitInSeconds * 1000) {
-            cout << "시간 제한 초과! 프로그램 종료합니다." << endl;
-            return 1;
-        }
+            // 시간 제한을 초과하면 종료합니다.
+            if (elapsedTime >= timeLimitInSeconds * 1000) {
+                cout << "시간 제한 초과! 프로그램 종료합니다." << endl;
+                return 1;
+            }
 
-        // 입력 이벤트를 비동기적으로 확인합니다.
-        if (PeekConsoleInput(hStdin, ir, 128, &bytesRead)) {
-            for (DWORD i = 0; i < bytesRead; i++) {
-                if (ir[i].EventType == KEY_EVENT && ir[i].Event.KeyEvent.bKeyDown) {
-                    userInput = ir[i].Event.KeyEvent.uChar.AsciiChar;
-                    cout << "입력한 문자: " << userInput << endl;
-                    return 0;
+            // 입력 이벤트를 비동기적으로 확인합니다.
+            if (PeekConsoleInput(hStdin, ir, 128, &bytesRead)) {
+                for (DWORD i = 0; i < bytesRead; i++) {
+                    if (ir[i].EventType == KEY_EVENT && ir[i].Event.KeyEvent.bKeyDown) {
+                        userInput = ir[i].Event.KeyEvent.uChar.AsciiChar;
+                        cout << "입력한 문자: " << userInput << endl;
+                        return 0;
+                    }
                 }
             }
+
+            // 입력 이벤트가 없으면 잠시 대기합니다.
+            Sleep(100);
         }
 
-        // 입력 이벤트가 없으면 잠시 대기합니다.
-        Sleep(100);
     }
-
     return 0;
 }
