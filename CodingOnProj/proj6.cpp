c9ee4125e51278693d05afc4982fb9b5b3c0caf6
moyasuri <moyasuri.hana@gmail.com>
2023-09-19 19:29:04 +0900

    proj6 complete

--- CodingOnProj/proj6.cpp
+++ CodingOnProj/proj6.cpp
@@ -1,59 +1,160 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstdlib>
-#include <ctime>
+
 using namespace std;
 
 
-
-//1. 클래스 상속, 정적 멤버, 업 캐스팅, 가상 함수를 사용하여 다음과 같이 간단한 사칙연산을 수행하는 계산기 만들기
-
-class Calc
-{
+class Calculator {
+public:
+    virtual double calculate(double a, double b) = 0;
 
 public:
-	virtual void Add() = 0;
+    static double _result;
+};
 
-	virtual void Sub() = 0;
-
-	virtual void Mul() = 0;
-
-	virtual void Div() = 0;
-
-
-
+class Add : public Calculator {
 public:
-	string _operator;
-	string _cmd;
-	int _divider;
-	static double _result;
-
+    double calculate(double a, double b) override {
+        return a + b;
+    }
 };
-class C_Add : public Calc
-{
+
+class Subtract : public Calculator {
 public:
-	double Add() {
-		return
-
-	}
-
-
-
+    double calculate(double a, double b) override {
+        return a - b;
+    }
 };
-class C_Sub : public Calc
-{
 
+class Multiply : public Calculator {
+public:
+    double calculate(double a, double b) override {
+        return a * b;
+    }
 };
-class C_Mul : public Calc
-{
 
+class Divide : public Calculator {
+public:
+    double calculate(double a, double b) override {
+        if (b != 0)
+            return a / b;
+        else
+            throw std::runtime_error("Error: Division by zero");
+    }
 };
-class C_Div : public Calc
-{
 
-};
-int main()
-{
 
+double Calculator::_result = 0;
+int main() {
+    
+    
+    
+    char operation;
+    bool loop = true, loop_cmd = true, loop_cmd2 = true;
+    
+    Calculator* calculator = nullptr;
+    double num1, num2;
+    string cmd = "first";
+    while (loop)
+    {
+        if (cmd.compare("first")==0)
+        {
+
+        }
+        else
+        {
+            cout << "연산을 계속하시겠습니까 ? (Y: 계속, AC : 초기화, EXIT : 종료) ";
+            cin >> cmd;
+        }
+        
+
+        
+        while (loop_cmd)
+        {
+
+
+            if (cmd.compare("Y")==0)
+            {
+                loop_cmd = false;
+                
+            }
+            else if (cmd.compare("AC")==0)
+            {
+                loop_cmd = false;
+                Calculator::_result = 0;
+            }
+            else if (cmd.compare("EXIT")==0)
+            {
+                return 0;
+            }
+            else if (cmd.compare("first")==0)
+            {
+                loop_cmd = false;
+            }
+            else
+            {
+                cout << "잘못된 명령어 입니다.";
+            }
+        }
+        if (cmd.compare("Y")==0)
+        {
+            num1 = Calculator::_result;
+        }
+        else
+        {
+            cout << "숫자를 입력해주세요 : ";
+            cin >> num1;
+        }
+        
+        std::cout << "연산자를 입력해주세요 (+, -, *, /): ";
+        cin >> operation;
+        cout << "숫자를 입력해주세요 : " ;
+        cin >> num2;
+
+        while (loop_cmd2)
+        {
+            switch (operation) {
+            case '+':
+                calculator = new Add();
+                Calculator::_result = calculator->calculate(num1, num2);
+                loop_cmd2 = false;
+
+                break;
+            case '-':
+                calculator = new Subtract();
+                Calculator::_result = calculator->calculate(num1, num2);
+                loop_cmd2 = false;
+
+                break;
+            case '*':
+                calculator = new Multiply();
+                Calculator::_result = calculator->calculate(num1, num2);
+                loop_cmd2 = false;
+
+                break;
+            case '/':
+                calculator = new Divide();
+                Calculator::_result = calculator->calculate(num1, num2);
+                loop_cmd2 = false;
+
+                break;
+            default:
+                std::cout << "Invalid operation" << std::endl;
+            }
+
+
+        }
+
+        calculator = nullptr;
+        loop_cmd = true;
+        loop_cmd2 = true;
+        cmd = "";
+        cout << "결과 : " << Calculator::_result << endl;
+        
+        delete calculator; // 동적 할당 해제
+
+
+    }
+
+
+    return 0;
 }
